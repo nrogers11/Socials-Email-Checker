0babcf221136f75d9f47f7c78498a7bcaf8ee228
Mason <123887095+cyclothymia@users.noreply.github.com>
2024-03-18 12:27:31 +1100

    Create main.py
    Created main.py that simplified previous commits into an all-in-one multiprocessing email checker.

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import asyncio
+
+from src.tools import Tools
+from src.proxy import workingProxy
+from multiprocessing import Pool, freeze_support
+from requests.structures import CaseInsensitiveDict
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print("[ GitHub ++ https://github.com/cyclothymia/Socials-Email-Checker ]")
+
+def run_async(email):
+    async def main(email):
+        tools = Tools()
+        await tools.instagram(email)
+        await tools.twitter(email)
+
+    asyncio.run(main(email))
+
+def process_email(line):
+    email = line.strip()
+    run_async(email)
+
+if __name__ == '__main__':
+    freeze_support()
+    filename = input("Enter the email filename: ")
+    
+    with open(filename) as f:
+        emails = f.readlines()
+
+    with Pool(processes=100) as pool:
+        pool.map(process_email, emails)
